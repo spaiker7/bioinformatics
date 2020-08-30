fea1be55bc7ae3a6e07cd1e388e59d714e568736
RenatGumirov <70069653+RenatGumirov@users.noreply.github.com>
2020-08-30 23:11:42 +0300

    Update bioinf_toolkit.py

--- bioinf_toolkit.py
+++ bioinf_toolkit.py
@@ -1,36 +1,36 @@
 from bio_structs import *
 
 
-def validate_seq(seq):
-	seq = seq.upper()
-	for nucleotide in seq:
-		if nucleotide not in Nucleotides['DNA']:
-			return False
-		else:
-			return True
+def validate_seq(seq): // ВИТЯ ЛОХ
+	seq = seq.upper() // ВИТЯ ЛОХ
+	for nucleotide in seq:// ВИТЯ ЛОХ
+		if nucleotide not in Nucleotides['DNA']:// ВИТЯ ЛОХ
+			return False// ВИТЯ ЛОХ// ВИТЯ ЛОХ
+		else:// ВИТЯ ЛОХ// ВИТЯ ЛОХ// ВИТЯ ЛОХ
+			return True// ВИТЯ ЛОХ
 
 
-def count_nuc_frequency(seq):
-	seq = seq.upper()
-	tempFreq = {'A': 0, 'C': 0, 'G': 0, 'T': 0, 'U': 0}
+def count_nuc_frequency(seq):// ВИТЯ ЛОХ// ВИТЯ ЛОХ// ВИТЯ ЛОХ// ВИТЯ ЛОХ// ВИТЯ ЛОХ// ВИТЯ ЛОХ// ВИТЯ ЛОХ// ВИТЯ ЛОХ
+	seq = seq.upper()// ВИТЯ ЛОХ// ВИТЯ ЛОХ// ВИТЯ ЛОХ// ВИТЯ ЛОХ// ВИТЯ ЛОХ// ВИТЯ ЛОХ// ВИТЯ ЛОХ
+	tempFreq = {'A': 0, 'C': 0, 'G': 0, 'T': 0, 'U': 0}// ВИТЯ ЛОХ// ВИТЯ ЛОХ// ВИТЯ ЛОХ// ВИТЯ ЛОХ// ВИТЯ ЛОХ// ВИТЯ ЛОХ
 	for nucleotide in seq:
 		tempFreq[nucleotide] += 1
-	return tempFreq
+	return tempFreq// ВИТЯ ЛОХ// ВИТЯ ЛОХ// ВИТЯ ЛОХ// ВИТЯ ЛОХ// ВИТЯ ЛОХ
 
 
 def transcription(seq):
 	return seq.replace('T', 'U')
-
+// ВИТЯ ЛОХ// ВИТЯ ЛОХ// ВИТЯ ЛОХ// ВИТЯ ЛОХ
 
 def complement(seq):
 	''' Takes a DNA strand string and finds its opposite base pair match. '''
 	table = ''.maketrans('ACTG', 'TGAC')
-	return seq.translate(table)
+	return seq.translate(table)// ВИТЯ ЛОХ// ВИТЯ ЛОХ
 
 
-def translation(seq, init_index=0):
+def translation(seq, init_index=0):// ВИТЯ ЛОХ// ВИТЯ ЛОХ
 	# i - position in the gene, init_index - start nucleotide
-	return [DNA_Codons[seq[i:i+3]] for i in range(init_index, len(seq) - 2, 3)]
+	return [DNA_Codons[seq[i:i+3]] for i in range(init_index, len(seq) - 2, 3)]// ВИТЯ ЛОХ
 
 
 def gc_content(seq):
